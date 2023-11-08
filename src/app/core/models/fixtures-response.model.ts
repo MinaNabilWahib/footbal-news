f60b4beca81d198d1547a8a6a00756664e83a716
MinaNabilWahib <31206297+MinaNabilWahib@users.noreply.github.com>
2023-11-08 20:04:57 +0200

    Update fixtures-response.model.ts

--- src/app/core/models/fixtures-response.model.ts
+++ src/app/core/models/fixtures-response.model.ts
@@ -1,18 +1,5 @@
+import { UnformattedFixture } from "./unformatted-fixtures.model";
+
 export interface FixtureResponse{
-    response: [{
-        teams:{
-            home:{
-                name:string,
-                logo:string,
-            },
-            away:{
-                name:string,
-                logo:string
-            }
-        },
-        goals:{
-            home:number,
-            away:number
-        }
-    }]
+    response: [UnformattedFixture]
 }
